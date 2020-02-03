43b34653b3755019ab0d43a957fd82db0139cddb
53417 <seanimal@Live.com.au>
2020-02-03 23:14:11 +1100

    basic auth working mlab backend, local host front

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: "mongodb+srv://seanimal:Seanjoin2017@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority" 
+};
+
+module.exports = {
+    mongoURI: "mongodb+srv://seanimal:Seanjoin2017@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
