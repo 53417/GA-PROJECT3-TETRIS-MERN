7b8b8f29e0c7d80780a44b167e67391e738d5d62
53417 <seanimal@Live.com.au>
2020-02-13 20:57:03 +1100

    VH fix for heroku

--- client/src/components/single/S4.js
+++ client/src/components/single/S4.js
@@ -1202,7 +1202,7 @@ export default class Single extends React.Component {
 
     render() {
         return (
-            <div style={{ height: "85vh" }} className="container valign-wrapper">
+            <div style={{ height: "80vh" }} className="container valign-wrapper">
                 <div className="row">
                     <div className="col s6 center-align">                         
                         <canvas id="canvas" width="250" height="500"></canvas>
