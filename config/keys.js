cad98943a252ab5a9d165522e7a2bb38e7fa44fa
53417 <48437598+53417@users.noreply.github.com>
2020-02-29 16:56:33 +1100

    oops

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-    mongoURI: "mongodb+srv://seanimal:Seanjoin2017@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://username:password@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority" 
 };
 
 module.exports = {
-    mongoURI: "mongodb+srv://seanimal:Seanjoin2017@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://username:password@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
