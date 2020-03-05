6ac66171fd4db9504f7094a43c0fcbbe1f479436
53417 <48437598+53417@users.noreply.github.com>
2020-03-06 10:22:22 +1100

    logged

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-    mongoURI: "mongodb+srv://username:password@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://seanimal@live.com.au:Seanjoin2017@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority" 
 };
 
 module.exports = {
-    mongoURI: "mongodb+srv://username:password@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://seanimal@live.com.au:Seanjoin2017@p3-zylcm.mongodb.net/test?retryWrites=true&w=majority" 
     secretOrKey: "secret"
 };
